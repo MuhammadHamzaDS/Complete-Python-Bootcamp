7bf337beae6cd37fb0db4c8094e517b9ceb34761
MuhammadHamzaDS <hamzaali709x@gmail.com>
2026-04-07 01:20:16 +0500

    Updated

---
+++ DSA/binary_search.py
@@ -0,0 +1,27 @@
+def binary_search(arr, key):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == key:
+            return mid
+        elif arr[mid] < key:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+
+# Example
+arr = [10, 20, 30, 40, 50, 60]
+key = 40
+
+result = binary_search(arr, key)
+
+if result != -1:
+    print("Element found at index:", result)
+else:
+    print("Element not found")
